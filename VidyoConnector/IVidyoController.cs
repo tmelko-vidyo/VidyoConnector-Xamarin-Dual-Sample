eefa7bdaa144b0f17bdd0d20660da6e840e9ed79
tmelko-vidyo <tmelko@vidyo.com>
2019-02-06 13:07:37 +0200

    Initial: Main VidyoConnector part

---
+++ VidyoConnector/IVidyoController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace VidyoConnector
+{
+    public interface IVidyoController
+    {
+
+        // Create connector instance and return client version
+        String Construct(Controls.NativeView videoView);
+
+        // Release connector instance
+        void Destruct();
+
+        // Page high-level lifecycle events
+        void OnAppResume();
+        void OnAppSleep();
+
+        // Fetch connector state
+        VidyoConnectorState ConnectorState { get; set; }
+
+        // Events triggered by button clicks from UI
+        bool Connect(string host, string token, string displayName, string resourceId);
+        void Disconnect();
+
+        // Orientation has changed
+        void RefreshUI();
+
+        void SetCameraPrivacy(bool privacy);
+        void SetMicrophonePrivacy(bool privacy);
+
+        void CycleCamera();
+
+        void EnableDebugging();
+        void DisableDebugging();
+    }
+}
