bd0e8e60f2831e2e78c61bcfbb97c59f8bbe76f9
tmelko-vidyo <tmelko@vidyo.com>
2020-06-30 15:34:14 +0300

    + VidyoClient 20.1.0.9 + Android - request permissions.

--- VidyoConnector.Android/MainActivity.cs
+++ VidyoConnector.Android/MainActivity.cs
@@ -1,7 +1,11 @@
 ﻿using System;
 using Android.App;
 using Android.Content.PM;
+using Android.Content;
 using Android.OS;
+using Android.Support.V4.Content;
+using Android;
+using Android.Support.V4.App;
 
 namespace VidyoConnector.Android
 {
@@ -13,6 +17,9 @@ namespace VidyoConnector.Android
     ScreenOrientation = ScreenOrientation.Portrait)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
+
+        private static int PERMISSION_REQUEST_FALLBACK = 34;
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
             TabLayoutResource = Resource.Layout.Tabbar;
@@ -22,6 +29,8 @@ namespace VidyoConnector.Android
 
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
 
+            ChecPermissions();
+
             try
             {
                 LoadApplication(new App(VidyoController.GetInstance()));
@@ -30,6 +39,45 @@ namespace VidyoConnector.Android
             {
                 Logger.GetInstance().Log(e.Message);
             }
+        }
+
+        private void ChecPermissions()
+        {
+            if (hasPermissionsGranted())
+            {
+                Logger.GetInstance().Log("All permissions are granted!");
+            }
+            else
+            {
+                ActivityCompat.RequestPermissions(this, new String[] { Manifest.Permission.Camera, Manifest.Permission.RecordAudio }, PERMISSION_REQUEST_FALLBACK);
+            }
+        }
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, Permission[] grantResults)
+        {
+            if (requestCode == PERMISSION_REQUEST_FALLBACK)
+            {
+                Logger.GetInstance().Log("Received response for Camera/Audio permission request.");
+
+                if (hasPermissionsGranted())
+                {
+                    Logger.GetInstance().Log("All permissions are granted!");
+                }
+                else
+                {
+                    Logger.GetInstance().Log("All permissions are NOT granted!");
+                }
+            }
+            else
+            {
+                base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+            }
+        }
+
+        private bool hasPermissionsGranted()
+        {
+            return ContextCompat.CheckSelfPermission(this, Manifest.Permission.Camera) == (int)Permission.Granted
+                && ContextCompat.CheckSelfPermission(this, Manifest.Permission.RecordAudio) == (int)Permission.Granted;
         }
     }
 }
